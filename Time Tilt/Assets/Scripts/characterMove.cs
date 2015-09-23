bdab365b6480516dd52df45f8cb72038d343a312
JamesC44 <jcc44ra@gmail.com>
2015-09-23 18:06:47 -0400

    added destory for player and enemy, also added turing left and right for player

---
+++ Time Tilt/Assets/Scripts/characterMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterMove : MonoBehaviour {
+    public GameObject target;
+    public float Speed = 1f;
+    public float still = 0f;
+
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey (KeyCode.A)) {
+			//look left 
+			transform.right = new Vector3 (-1, 0, 0);
+			//move left
+			transform.Translate (Vector3.right * Speed);
+		} else if (Input.GetKey (KeyCode.D)) {
+			//move right
+			transform.Translate (Vector3.right * Speed);
+			//look right
+			transform.right = new Vector3(1,0,0);
+		} else
+            transform.Translate(Vector3.zero * still);
+
+    }
+	
+}
---
+++ Time Tilt/Assets/Scripts/destroyPlayer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyPlayer : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.name == "enemy01") {
+			Destroy (GameObject.FindWithTag("Player"));
+		}
+	} 
+}
