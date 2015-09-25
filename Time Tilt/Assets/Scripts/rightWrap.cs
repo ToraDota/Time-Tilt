8d99047bae5c299d9ead86c6733b6fa3bf8b95fa
JamesC44 <jcc44ra@gmail.com>
2015-09-25 15:57:07 -0400

    screen wrap

---
+++ Time Tilt/Assets/Scripts/rightWrap.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rightWrap : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player" || other.tag == "Enemy") {	
+			other.gameObject.transform.position = new Vector3 (-7.3f, other.gameObject.transform.position.y, other.gameObject.transform.position.z);
+		
+
+		}
+	}
+
+
+
+
+
+}
