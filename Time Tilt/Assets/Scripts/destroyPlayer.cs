1211709e207aa58bdea36486b15064c620e8c208
JamesC44 <jcc44ra@gmail.com>
2015-10-23 15:26:39 -0400

    Respawn working correctly

--- Time Tilt/Assets/Scripts/destroyPlayer.cs
+++ Time Tilt/Assets/Scripts/destroyPlayer.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class destroyPlayer : MonoBehaviour
 {
-	public Renderer rend;
+	public Renderer rendHead;
+	public Renderer rendBottom;
+	public Renderer rendLance;
+	public Renderer rendBodyBack;
+	public Renderer rendBodyFront;
 	public Transform Respawn;
 	public GameObject Player;
 	int Lives = 3;
@@ -26,12 +30,21 @@ public class destroyPlayer : MonoBehaviour
 		Debug.Log ("dead");
 		flag = 2;
 		Player = GameObject.FindWithTag ("Player");
-		rend.enabled = false;
+		Player.transform.position = new Vector3 (-11, 0, 0);
+		rendHead.enabled = false;
+		rendLance.enabled = false;
+		rendBottom.enabled = false;
+		rendBodyBack.enabled = false;
+		rendBodyFront.enabled = false;
 		yield return new WaitForSeconds (2);
 		Debug.Log ("respawn");
 		flag =1;
 		Player.transform.position = Respawn.transform.position;
-		rend.enabled = true;
+		rendHead.enabled = true;
+		rendLance.enabled = true;
+		rendBottom.enabled = true;
+		rendBodyBack.enabled = true;
+		rendBodyFront.enabled = true;
 		}
 }
 
