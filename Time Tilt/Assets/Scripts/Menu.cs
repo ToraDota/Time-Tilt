d308a361e63390cea091a51ca279108b33be8ce7
cHricik <chricik@knights.ucf.edu>
2015-10-24 22:24:48 -0400

    Menu and enemy follow

---
+++ Time Tilt/Assets/Scripts/Menu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public Button startText;
+    public Button exitText;
+
+
+	// Use this for initialization
+	void Start () {
+	
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+
+	}
+
+    public void ExitPress()
+    {
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Time Tilt/Assets/Scripts/enemyMove.cs
+++ Time Tilt/Assets/Scripts/enemyMove.cs
@@ -3,35 +3,70 @@ using System.Collections;
 
 public class enemyMove : MonoBehaviour
 {
-
-	public static System.Random rnd = new System.Random ();
+    public int i;
+    public float flapforce= 1f;
+    public ForceMode2D forceMode = ForceMode2D.Impulse;
+    private float nextActionTime = 0.1f;
+    public float period = .1f;
+    public Transform target;
+    public int rotationSpeed;
+    public int maxdistance;
+    private Transform myTransform;
+    public static System.Random rnd = new System.Random ();
 	public static int direction =  1;  //rnd.Next (0, 2);
-	public float enemySpeed = 3f;
+	public float enemySpeed = 3f; 
 
-	// Use this for initialization
-	void Start ()
-	{
-     
-	}
+    void Awake()
+    {
+        myTransform = transform;
+        if (direction == 0)
+            transform.Translate(Vector3.left * enemySpeed * Time.deltaTime);
+        else
+            transform.Translate(Vector3.right * enemySpeed * Time.deltaTime);
+    }
+
+    void Start()
+    {
+
+        maxdistance = 0;
+    }
+
 
 	// Update is called once per frame
 	void Update ()
 	{
-		if (direction == 0)
-			transform.Translate (Vector3.left * enemySpeed * Time.deltaTime);
-		else
-			transform.Translate (Vector3.right * enemySpeed * Time.deltaTime);
+        //Follow script
+        if (Vector3.Distance(target.position, myTransform.position) > maxdistance)
+        {
+            // Get a direction vector from us to the target
+            Vector3 dir = target.position - myTransform.position;
 
-		Physics2D.IgnoreLayerCollision(8,9);
+            // Normalize it so that it's a unit direction vector
+            dir.Normalize();
+
+            // Move ourselves in that direction
+            myTransform.position += dir * enemySpeed * Time.deltaTime;
+        }
+        //tried to make a flap script, not sure what's actually wrong with it.
+        if (Time.time >= nextActionTime)
+        {
+            nextActionTime = Time.time + period;
+            for (i = 0; i < 10; i++)
+            {
+                GetComponent<Rigidbody2D>().AddForce(Vector2.up * flapforce, forceMode);
+            }
+        }
+
+        Physics2D.IgnoreLayerCollision(8,9);
 	}
 
 	//destroy
-	/*void OnTriggerEnter2D (Collider2D col)
+	void OnTriggerEnter2D (Collider2D col)
 	{
 		if (col.name == "Lance") {
 			Destroy (gameObject);
 		}
-	}*/
+	}
 
 
 }
